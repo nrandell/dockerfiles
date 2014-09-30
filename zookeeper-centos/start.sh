b6d6b48fbba7d13b032813f02dca56b34e771128
Nick Randell <nick@nickrandell.com>
2014-09-30 09:42:54 +0100

    Updated start

--- zookeeper-centos/start.sh
+++ zookeeper-centos/start.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 
+env
 ZOOKEEPER_DIR=/opt/zookeeper-3.4.6
 cp $ZOOKEEPER_DIR/conf/zoo.cfg.template $ZOOKEEPER_DIR/conf/zoo.cfg
+OIFS=$IFS
 IFS=','
 ID=1
 for server in $ZK_SERVERS
@@ -9,11 +11,13 @@ do
   echo "server.$ID=$server:2888:3888" >> $ZOOKEEPER_DIR/conf/zoo.cfg
   ID=$(expr $ID + 1)
 done
+IFS=$OIFS
 cat $ZOOKEEPER_DIR/conf/zoo.cfg
 
 
-printf "${ZK_SERVER_NUMBER}" > /data/myid
-printf "Starting zookeeper server $(cat /data/myid)\n"
+echo "Building myid as server $ZK_SERVER_NUMBER"
+echo $ZK_SERVER_NUMBER > /data/myid
+echo "Starting zookeeper server $(cat /data/myid)"
 export JMXDISABLE=true
 
 $ZOOKEEPER_DIR/bin/zkServer.sh start-foreground
